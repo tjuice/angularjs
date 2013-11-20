e2547ef8e03ee7819e7b2ebbbab52d8a015651b9
Thomas Pannek <thomas.pannek@gmail.com>
2013-11-20 15:22:23 +0100

    String mappings added

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,5 +1,5 @@
 // setter
-var app = angular.module("app",[]).config(function($routeProvider) {
+var app = angular.module("app",[]).config(['$routeProvider' , function($routeProvider) {
 	$routeProvider.when('/login', {
 		templateUrl: 'login.html',
 		controller: 'loginController'
@@ -9,9 +9,9 @@ var app = angular.module("app",[]).config(function($routeProvider) {
 		controller: 'homeController'
 	})
 	$routeProvider.otherwise({redirectTo : '/login' })
-});
+}]);
 
-app.factory("AuthenticationService", function($location) {
+app.factory("AuthenticationService",  ['$location', function($location) {
 	return {
 		login: function(credentials) {
 			if(credentials.username === "ralph") {
@@ -22,22 +22,22 @@ app.factory("AuthenticationService", function($location) {
 			$location.path('/login');
 		} 
 	};
-});
+}]);
 
-app.controller('loginController', function($scope, AuthenticationService) {
+app.controller('loginController', ['$scope','AuthenticationService', function( $scope, AuthenticationService) {
 	$scope.credentials = { username: "", password: "" };
 	$scope.login = function() {
 		AuthenticationService.login($scope.credentials);
 	}
-});
+}]);
 
-app.controller('homeController', function($scope, AuthenticationService) {
+app.controller('homeController', ['$scope','AuthenticationService', function( $scope, AuthenticationService) {
 	$scope.title = "home";
 	$scope.message = "mouse over these Images";
 	$scope.logout = function() {
 		AuthenticationService.logout();
 	}
-});
+}]);
 
 app.directive('showsMessageWhenHovered',function(){
 	return {
